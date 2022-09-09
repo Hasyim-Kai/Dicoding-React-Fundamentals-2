ce6b1ff969ddffe0cec507e457793abc19b8bf0c
ichihatsu1 <ichihatsu1@gmail.com>
2022-09-09 21:44:36 +0700

    add 404 Page

---
+++ src/presentation/pages/Page404.tsx
@@ -0,0 +1,6 @@
+export default function Page404() {
+  return <main className="mt-24">
+    <h1 className="text-4xl text-center">404</h1>
+    <h1 className="text-4xl text-center">Nyasar Nich ..</h1>
+  </main>
+}
