7dceaac9542d332f92ecdafad2922f969a22cc47
ichihatsu1 <ichihatsu1@gmail.com>
2022-09-10 09:20:27 +0700

    Add Empty Notes Component

---
+++ src/presentation/components/EmptyNotes.tsx
@@ -0,0 +1,4 @@
+export const EmptyNotes = () => <section className={`text-center mt-16`}>
+    <h1 className='text-4xl'>Notes Empty ..</h1>
+    <h1 className='text-xl mt-2'>Create some Notes</h1>
+</section>
