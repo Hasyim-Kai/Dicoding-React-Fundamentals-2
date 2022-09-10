53521631ed17f4c8bf822a6062581119995ab484
ichihatsu1 <ichihatsu1@gmail.com>
2022-09-10 09:16:24 +0700

    Refactor 404 Page

--- src/presentation/pages/Page404.tsx
+++ src/presentation/pages/Page404.tsx
@@ -1,6 +1,6 @@
 export default function Page404() {
-  return <main className="mt-24">
-    <h1 className="text-4xl text-center">404</h1>
-    <h1 className="text-4xl text-center">Nyasar Nich ..</h1>
+  return <main className="mt-32">
+    <h1 className="text-9xl text-center">404</h1>
+    <h1 className="text-3xl text-center mt-5"><i>Nyasar Nich ..</i></h1>
   </main>
 }
